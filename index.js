715aee9164639e1de4811eea12e409d0aca0a112
jasonzhoujc <jason.zhoujc@gmail.com>
2013-11-29 08:53:58 +0800

    qunit for js

---
+++ index.js
@@ -0,0 +1,34 @@
+var ops = {
+    // Unit tests.
+    qunit: {
+        all: ['test/qunit1.html']/*,
+        all_tests: ['test/*{1,2}.html'],
+        individual_tests: {
+            files: [
+                {src: 'test/*1.html'},
+                {src: 'test/*{1,2}.html'}
+            ]
+        }*/
+    }
+};
+// Build a mapping of url success counters.
+debugger
+var successes = {};
+var currentUrl;
+// External lib.
+var EventEmitter2 = require('eventemitter2').EventEmitter2;
+var path = require('path');
+var asset = path.join.bind(null, __dirname, '..');
+
+// Awesome.
+var myevent = module.exports = new EventEmitter2({wildcard: true});
+myevent.on('qunit.spawn', function(url) {
+    currentUrl = url;
+    if (!successes[currentUrl]) { successes[currentUrl] = 0; }
+});
+myevent.on('qunit.done', function(failed, passed) {
+    if (failed === 0 && passed === 2) { successes[currentUrl]++; }
+});
+
+var myQunit = require('./lib/myQunit.js');
+myQunit(myevent, ops);
